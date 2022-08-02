7128f4241ccd81dd923472800893c73550aa6aef
puszekjuliuszek <tojabonibykto@gmail.com>
2022-08-02 23:04:38 +0200

    I've made these tests

--- Pytest/Task1/test_function.py
+++ Pytest/Task1/test_function.py
@@ -1,2 +1,11 @@
+import pytest
 from function import square
 # Napisać cztery testy z dla funckji square dla 0, 1, -1 i 100
+def testA():
+    assert square(0) == 0
+def testB():
+    assert square(1) == 1
+def testC():
+    assert square(-1) == 1
+def testD():
+    assert square(100) == 10000
